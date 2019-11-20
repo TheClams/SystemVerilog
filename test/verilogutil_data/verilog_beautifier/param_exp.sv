545b8d3a26861e5daf3fe4f485704ba8d5c69d14
Christophe Devaucelle <cdevaucelle@semtech.com>
2019-11-20 13:18:11 +0100

    [Alignment] Support real parameters
    Add relevant test
    
    Remove sublime stub: outdated and never used ...

--- test/verilogutil_data/verilog_beautifier/param_exp.sv
+++ test/verilogutil_data/verilog_beautifier/param_exp.sv
@@ -1,7 +1,8 @@
 module dcs_packet_rx_v2 #(
-    parameter      pDataWidth = 20           ,
-    parameter      pBaud      = 115_200      ,
-    localparam int lpBaud     = $clog2(pBaud)
+    parameter       pDataWidth = 20           ,
+    parameter       pBaud      = 115_200      ,
+    parameter  time t          = 1.5e-3       ,
+    localparam int  lpBaud     = $clog2(pBaud)
 ) (
     input iClk
 );
