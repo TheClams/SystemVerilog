e16ed5d3bd48576471f9f76ac7578ffd0eda791a
TheClams <clams@mail.com>
2020-04-18 08:12:52 +0200

    [Alignement] Fix issue with sign-ness of parameters

--- test/verilogutil_data/verilog_beautifier/param.sv
+++ test/verilogutil_data/verilog_beautifier/param.sv
@@ -1,5 +1,5 @@
 module dcs_packet_rx_v2 #(
-parameter pDataWidth = 20,
+parameter int unsigned pDataWidth = 20,
 parameter pBaud = 115_200,
 parameter  time t= 1.5e-3,
 localparam int lpBaud = $clog2(pBaud)
